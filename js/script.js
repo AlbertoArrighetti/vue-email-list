4bfa305e829f3198f663c07a089160a7dfaf7710
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-21 16:48:02 +0100

    bonus 1 completato con utilizzo di Promise.all

--- js/script.js
+++ js/script.js
@@ -11,15 +11,21 @@ createApp({
     methods: {
         // creo una funzione per generare 10 email
         emailsGen() {
+            // genero un array che mi tenga in "standby" le email finchè non sono tutte generate 
+            const provisionalEmails = [];
+
             for(let i = 0; i < 10; i++) {
-
-                // per ogni iterazione genero una mail dall'API 
-
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
-                    // la pusho nel mio array 
-                    this.emailList.push(res.data.response);
-                })
+                // per ogni iterazione genero una mail dall'API  e la salvo nell'array precente 
+                provisionalEmails.push(axios.get('https://flynn.boolean.careers/exercises/api/random/mail')); 
             }
+
+            // tramite il Promise.all aspetto che tutte le iterazioni avvengano 
+            // per poi inserire tutte le email generate nel mio array da far visualizzare 
+            Promise.all(provisionalEmails).then((responses) =>{
+
+                this.emailList = responses.map((res) => res.data.response);
+            })
+
         }
     },
     
