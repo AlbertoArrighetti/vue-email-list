d33832968163bccdfcd186a1b1e71a0b45ac50f1
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-21 16:13:45 +0100

    lista con 10 emails randomiche

--- js/script.js
+++ js/script.js
@@ -3,18 +3,29 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            randomEmail: '',
+            // creo un array vuoto in cui conterrò tutte le mie emails
+            emailList: [],
+        }
+    },
+
+    methods: {
+        // creo una funzione per generare 10 email
+        emailsGen() {
+            for(let i = 0; i < 10; i++) {
+
+                // per ogni iterazione genero una mail dall'API 
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    // la pusho nel mio array 
+                    this.emailList.push(res.data.response);
+                })
+            }
         }
     },
     
     // al caricamento della pagina:
     mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-            // così visualizzo la mail generata dall'APi
-            console.log(response.data.response);
-
-            this.randomEmail = response.data.response;
-        })
-        
+        // richiamo la mia funzione
+        this.emailsGen();
     },
 }).mount('#app');
