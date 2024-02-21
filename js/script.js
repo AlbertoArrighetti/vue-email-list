7916f23af0e3e7283c2bd03275532f3660c42160
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-21 15:54:37 +0100

    elemento in pagina tramite API

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-     
+            randomEmail: '',
         }
     },
     
     // al caricamento della pagina:
     mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+            // così visualizzo la mail generata dall'APi
+            console.log(response.data.response);
+
+            this.randomEmail = response.data.response;
+        })
         
-        
-    }
+    },
 }).mount('#app');
