0f6b3df9ff290ef5d6de5bb49003513bb1da4aea
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-21 15:45:58 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+     
+        }
+    },
+    
+    // al caricamento della pagina:
+    mounted() {
+        
+        
+    }
+}).mount('#app');
